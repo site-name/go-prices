ebe222803e192f8de081b5605d258f967e646cbb
Son Le <leminhson2398@outlook.com>
2024-07-18 08:05:53 +0700

    Update common_test.go

--- common_test.go
+++ common_test.go
@@ -11,7 +11,7 @@ func TestQuantizePrice(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	res, err := QuantizePrice[*Money](money, Up)
+	res, err := QuantizePrice(money, Up)
 	if err != nil {
 		t.Fatal(err)
 	}
