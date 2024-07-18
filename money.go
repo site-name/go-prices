70d2c6d23e3bb5ead08ef6fe031de1870f44a300
Son Le <leminhson2398@outlook.com>
2024-07-18 08:05:06 +0700

    Update money.go

--- money.go
+++ money.go
@@ -8,14 +8,11 @@ import (
 
 // Money represents an amount of a particular currency.
 type Money struct {
-	Amount   decimal.Decimal
-	Currency string
+	amount   decimal.Decimal
+	currency string
 }
 
-var (
-	_ Currencier             = (*Money)(nil)
-	_ MoneyInterface[*Money] = (*Money)(nil)
-)
+var _ MoneyInterface[Money] = (*Money)(nil)
 
 // NewMoney returns new Money object
 func NewMoney(amount float64, currency string) (*Money, error) {
@@ -31,110 +28,107 @@ func NewMoneyFromDecimal(amount decimal.Decimal, currency string) (*Money, error
 		return nil, ErrMoneyNegative
 	}
 	return &Money{
-		Amount:   amount,
-		Currency: unit,
+		amount:   amount,
+		currency: unit,
 	}, nil
 }
 
 // String implements fmt.Stringer interface
-func (m *Money) String() string {
-	return fmt.Sprintf("Money{%s, %s}", m.Amount.String(), m.Currency)
+func (m Money) String() string {
+	return fmt.Sprintf("Money{%s, %s}", m.amount.String(), m.currency)
 }
 
-// GetCurrency returns current money's Currency
-func (m *Money) GetCurrency() string {
-	return m.Currency
+// GetCurrency returns current money's currency
+func (m Money) GetCurrency() string {
+	return m.currency
+}
+
+func (m Money) GetAmount() decimal.Decimal {
+	return m.amount
 }
 
 // LessThan checks if other's amount is greater than m's amount
 // AND checking same currency included
-func (m *Money) LessThan(other Money) bool {
-	return m.SameKind(other) && m.Amount.LessThan(other.Amount)
+func (m Money) LessThan(other Money) bool {
+	return m.SameKind(other) && m.amount.LessThan(other.amount)
 }
 
 // Equal checks if other's amount is equal to m's amount
-func (m *Money) Equal(other Money) bool {
-	return m.SameKind(other) && m.Amount.Equal(other.Amount)
+func (m Money) Equal(other Money) bool {
+	return m.SameKind(other) && m.amount.Equal(other.amount)
 }
 
 // LessThanOrEqual check if m's amount is less than or equal to other's amount
-func (m *Money) LessThanOrEqual(other Money) bool {
+func (m Money) LessThanOrEqual(other Money) bool {
 	return m.LessThan(other) || m.Equal(other)
 }
 
 // Mul multiplty current money with the givent other.
 //
 // NOTE: other must be either ints or floats or Decimal
-func (m *Money) Mul(other float64) Money {
+func (m Money) Mul(other float64) Money {
 	return Money{
-		Currency: m.Currency,
-		Amount:   m.Amount.Mul(decimal.NewFromFloat(other)),
+		currency: m.currency,
+		amount:   m.amount.Mul(decimal.NewFromFloat(other)),
 	}
 }
 
 // TrueDiv divides money with the given other.
 //
 // NOTE: other must be either ints or uints or floats or Decimal or Money
-func (m *Money) TrueDiv(other float64) Money {
+func (m Money) TrueDiv(other float64) Money {
 	return Money{
-		Currency: m.Currency,
-		Amount:   m.Amount.DivRound(decimal.NewFromFloat(other), int32(currencies[m.Currency].Fraction)),
+		currency: m.currency,
+		amount:   m.amount.DivRound(decimal.NewFromFloat(other), int32(currencies[m.currency].Fraction)),
 	}
 }
 
 // Add adds two money amount together, returns new money.
 // If returned error is not nil, it could be ErrNotSameCurrency
-func (m *Money) Add(other Money) (*Money, error) {
+func (m Money) Add(other Money) (*Money, error) {
 	if !m.SameKind(other) {
 		return nil, ErrNotSameCurrency
 	}
 
 	return &Money{
-		m.Amount.Add(other.Amount),
-		m.Currency,
+		m.amount.Add(other.amount),
+		m.currency,
 	}, nil
+}
+
+// Neg returns -m
+func (m Money) Neg() Money {
+	return Money{
+		amount:   m.amount.Neg(),
+		currency: m.currency,
+	}
 }
 
 // Sub subtracts current money to given other.
 // If error is not nil, it could be ErrNotSameCurrency
-func (m *Money) Sub(other Money) (*Money, error) {
-	if !m.SameKind(other) {
-		return nil, ErrNotSameCurrency
-	}
-
-	return &Money{
-		m.Amount.Sub(other.Amount),
-		m.Currency,
-	}, nil
+func (m Money) Sub(other Money) (*Money, error) {
+	return m.Add(m.Neg())
 }
-
-// func (m *Money) FlatTax(taxRate *decimal.Decimal, kepGross bool) {
-// 	faction := decimal.NewFromInt(1).Add(*taxRate)
-// 	if kepGross {
-// 		// net :=
-// 	}
-// 	d := decimal.NewFromInt(12)
-// }
 
 // Return a copy of the object with its amount quantized.
 // NOTE: if exp < 0, default will be used
-func (m *Money) Quantize(round Rounding, exp int) (*Money, error) {
+func (m Money) Quantize(round Rounding, exp int) (*Money, error) {
 	if exp < 0 {
-		exp, _ = GetCurrencyPrecision(m.Currency)
+		exp, _ = GetCurrencyPrecision(m.currency)
 	}
 
 	money := &Money{
-		Currency: m.Currency,
+		currency: m.currency,
 	}
 	switch round {
 	case Up:
-		money.Amount = m.Amount.RoundUp(int32(exp))
+		money.amount = m.amount.RoundUp(int32(exp))
 	case Down:
-		money.Amount = m.Amount.RoundDown(int32(exp))
+		money.amount = m.amount.RoundDown(int32(exp))
 	case Ceil:
-		money.Amount = m.Amount.RoundCeil(int32(exp))
+		money.amount = m.amount.RoundCeil(int32(exp))
 	case Floor:
-		money.Amount = m.Amount.RoundFloor(int32(exp))
+		money.amount = m.amount.RoundFloor(int32(exp))
 
 	default:
 		return nil, ErrInvalidRounding
@@ -144,23 +138,23 @@ func (m *Money) Quantize(round Rounding, exp int) (*Money, error) {
 }
 
 // Apply a fixed discount to Money type.
-func (m *Money) fixedDiscount(discount Money) (*Money, error) {
+func (m Money) fixedDiscount(discount Money) (*Money, error) {
 	sub, err := m.Sub(discount)
 	if err != nil {
 		return nil, err
 	}
 
-	if sub.Amount.GreaterThan(decimal.Zero) {
+	if sub.amount.GreaterThan(decimal.Zero) {
 		return sub, nil
 	}
 
 	return &Money{
-		Currency: m.Currency,
-		Amount:   decimal.Zero,
+		currency: m.currency,
+		amount:   decimal.Zero,
 	}, nil
 }
 
-func (m *Money) fractionalDiscount(fraction decimal.Decimal, _ bool) (*Money, error) {
+func (m Money) fractionalDiscount(fraction decimal.Decimal, _ bool) (*Money, error) {
 	mul := m.Mul(fraction.InexactFloat64())
 	quantized, err := mul.Quantize(Down, -1)
 	if err != nil {
