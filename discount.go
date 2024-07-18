10fa31b21479c6d9b4b2b031435e7d1ad6ea6e6a
Son Le <leminhson2398@outlook.com>
2024-07-18 08:06:38 +0700

    Update discount.go

--- discount.go
+++ discount.go
@@ -3,17 +3,17 @@ package goprices
 import "github.com/site-name/decimal"
 
 // FixedDiscount applys a fixed discount to any price type.
-func FixedDiscount[K MoneyObject, T MoneyInterface[K]](base T, discount Money) (K, error) {
+func FixedDiscount[K MoneyObject, T MoneyInterface[K]](base T, discount Money) (*K, error) {
 	return base.fixedDiscount(discount)
 }
 
 // FractionalDiscount Apply a fractional discount based on either gross or net amount
-func FractionalDiscount[K MoneyObject, T MoneyInterface[K]](base T, fraction decimal.Decimal, fromGross bool) (K, error) {
+func FractionalDiscount[K MoneyObject, T MoneyInterface[K]](base T, fraction decimal.Decimal, fromGross bool) (*K, error) {
 	return base.fractionalDiscount(fraction, fromGross)
 }
 
 // PercentageDiscount Apply a percentage discount based on either gross or net amount.
-func PercentageDiscount[K MoneyObject, T MoneyInterface[K]](base T, percentage float64, fromGross bool) (K, error) {
+func PercentageDiscount[K MoneyObject, T MoneyInterface[K]](base T, percentage float64, fromGross bool) (*K, error) {
 	factor := decimal.NewFromFloat(percentage).Div(decimal.NewFromFloat(100))
 	return base.fractionalDiscount(factor, fromGross)
 }
