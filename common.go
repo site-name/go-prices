c9f9d66e91c8db2487f67ab56e4f527bb1d23c6b
Son Le <leminhson2398@outlook.com>
2024-07-18 08:05:35 +0700

    Update common.go

--- common.go
+++ common.go
@@ -7,23 +7,23 @@ import (
 )
 
 type MoneyObject interface {
-	*Money |
-		*MoneyRange |
-		*TaxedMoney |
-		*TaxedMoneyRange
+	Money |
+		MoneyRange |
+		TaxedMoney |
+		TaxedMoneyRange
 }
 
-// MoneyInterface
 type MoneyInterface[T MoneyObject] interface {
-	Quantize(round Rounding, exp int) (T, error) // NOTE: if exp < 0, system wil use default
+	Quantize(round Rounding, exp int) (*T, error) // NOTE: if exp < 0, system wil use default
 	fmt.Stringer
-	fixedDiscount(discount Money) (T, error)
-	fractionalDiscount(fraction decimal.Decimal, fromGross bool) (T, error)
+	fixedDiscount(discount Money) (*T, error)
+	fractionalDiscount(fraction decimal.Decimal, fromGross bool) (*T, error)
 	GetCurrency() string
+	Neg() T
 }
 
 // QuantizePrice accepts the `price` argument to be either:
-// *TaxedMoney or *Money or *TaxedMoneyRange
-func QuantizePrice[K MoneyObject, T MoneyInterface[K]](price T, round Rounding) (K, error) {
+// TaxedMoney or Money or TaxedMoneyRange
+func QuantizePrice[K MoneyObject, T MoneyInterface[K]](price T, round Rounding) (*K, error) {
 	return price.Quantize(round, -1)
 }
